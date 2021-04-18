f7636166726ce6a8d14fb3bd0896ad893d0c2627
mkanno24 <79492650+mkanno24@users.noreply.github.com>
2021-04-18 16:20:57 -0400

    update date and time added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+function formatDay(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    minutes = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperatureDisplay");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#windSpeed");
   let descriptionElement = document.querySelector("#conditionDescription");
+  let dateElement = document.querySelector("#dayAndTime");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  dateElement.innerHTML = formatDay(response.data.dt * 1000);
 }
 let apiKey = "a5e503971f3cb1573fe109a966b147dd";
 let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
