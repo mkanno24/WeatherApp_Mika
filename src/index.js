8cfbb1742863a999242c7d09ee663063b6fd7293
mkanno24 <79492650+mkanno24@users.noreply.github.com>
2021-04-18 17:18:16 -0400

    City Search added

--- src/index.js
+++ src/index.js
@@ -24,11 +24,13 @@ function formatDay(timestamp) {
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperatureDisplay");
   let humidityElement = document.querySelector("#humidity");
+  let cityElement = document.querySelector("#city");
   let windElement = document.querySelector("#windSpeed");
   let descriptionElement = document.querySelector("#conditionDescription");
   let dateElement = document.querySelector("#dayAndTime");
   let iconElement = document.querySelector("#temperatureIcon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -42,7 +44,17 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
-let apiKey = "a5e503971f3cb1573fe109a966b147dd";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=Singapore&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "a5e503971f3cb1573fe109a966b147dd";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSearch(event) {
+  event.preventDefault();
+  let citySearchElement = document.querySelector("#citySearch");
+  search(citySearchElement.value);
+}
+
+let form = document.querySelector("#searchForm");
+form.addEventListener("submit", handleSearch);
